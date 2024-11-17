37eea4b7326d19d0612083f602da8e157f0dd8ee
catapam <matheuscatapam@gmail.com>
2024-11-17 16:39:28 +0000

    Install whitenoise for heroku static files setup

--- coffee_hub/settings.py
+++ coffee_hub/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    # Static file for heroku setup
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "coffee_hub.urls"
